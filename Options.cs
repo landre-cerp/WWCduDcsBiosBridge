949370bb371d305e8842cb5fefb13036563566ad
Cerppo <49897630+Cerppo@users.noreply.github.com>
2025-09-12 15:41:33 +0200

    Fix missing options parsing and added one to unlink screen brightness from other

--- Options.cs
+++ Options.cs
@@ -30,5 +30,14 @@ namespace WWCduDcsBiosBridge
         {
             Description = "Display CMS on the screen",
         };
+
+        /// <summary>
+        /// Option to Link ch47 screen Brightness
+        /// </summary>
+        public static Option<bool> CH47_LinkedBGBrightness = new("--ch47-linked-brightness", "-ch47lbg")
+        {
+            Description = "Link BG Brightness to other",
+        };
+
     }
 }
